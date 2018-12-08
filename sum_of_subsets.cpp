cd7bcf7fdd83c9dd3610becc2e79277580284d8a
ddochi93 <rangskim@naver.com>
2018-12-08 23:05:47 +0900

    Update sum_of_subsets.cpp

--- sum_of_subsets.cpp
+++ sum_of_subsets.cpp
@@ -1,3 +1,4 @@
+#pragma warning (disable:4996)
 #include <iostream>
 #include <stdio.h>
 #include <algorithm>
@@ -7,56 +8,46 @@ using namespace std;
 int w[1001] = { 0 , };
 //bool include[1001] = { false };
 const char * include[1001];
-int W = 0;    // ±¸ÇÏ·Á´Â ÃÑÇÕ.
+int W = 0;    // êµ¬í•˜ë ¤ëŠ” ì´í•©.
 void sum_of_subsets(int, int, int);
 bool promising(int, int, int);
 
 void sum_of_subsets(int i, int weight, int total) {
-	if (promising(i,weight,total)) {
+	if (promising(i, weight, total)) {
 		if (weight == W) {
-			for (int idx = 0; idx <= i; idx++)
-				cout<<  include[idx];
+			for (int idx = 1; idx <= i; idx++)
+				cout << include[idx] << " ";
 			cout << endl;
 		}
 		else {
-			//strcpy(include[i + 1], "yes");
-			//include[i + 1] = true;
 			include[i + 1] = "yes";
 			sum_of_subsets(i + 1, weight + w[i + 1], total - w[i + 1]);
-			//strcpy(include[i + 1], "no");
 			include[i + 1] = "no";
 			sum_of_subsets(i + 1, weight, total - w[i + 1]);
 		}
 	}
 }
 
-bool promising(int i,int weight, int total) {
+bool promising(int i, int weight, int total) {
 	return (weight + total >= W) && (weight == W || weight + w[i + 1] <= W);
 }
 
 
 int main(void) {
 	int n = 0;
-	int total = 0;  //total
-	printf("¿ø¼ÒÀÇ °¹¼ö(n)°ú ¸¸µå·Á´Â ÃÑÇÕ(W) ÀÔ·Â : ");
+	int total = 0;  
+	printf("ì›ì†Œì˜ ê°¯ìˆ˜(n)ê³¼ ë§Œë“œë ¤ëŠ” ì´í•©(W) ì…ë ¥ : ");
 	scanf("%d %d", &n, &W);
 
-	printf("¿ø¼Ò °¢°¢ ÀÔ·Â(%d°³) : ", n);
-	for (int i = 0; i < n; i++)
+	printf("ì›ì†Œ ê°ê° ì…ë ¥(%dê°œ) : ", n);
+	for (int i = 1; i <= n; i++)
 		scanf("%d", &w[i]);
-	sort(w, w + n);          //±³Àç ¾Ë°í¸®ÁòÀº ÀÌ°Å ±»ÀÌ¾ÈÇØµµ? 21¶óÀÎ
+	sort(w+1, w + n +1);          
 
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i <= n; i++)
 		total += w[i];
 
-	sum_of_subsets(-1, 0, total);
-	//const char * fuck = "fuck";
-	//printf("%s\n", fuck);
-	/*string str[5];
-	str[3] = "fucksdalkj";
-	for (int i = 0; i < 5; i++) {
-		cout << 5;
-	}*/
-
+	sum_of_subsets(0, 0, total);
+	getchar();
 	return 0;
 }
